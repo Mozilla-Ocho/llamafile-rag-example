Meeeee6623/containerized-llamafile-rag-example

add support for pdf files with PyPDF2

app.py
@@ -1,22 +1,32 @@
 import json
 import logging
-import re
 import traceback
 from pathlib import Path
 from typing import Iterator
 
+import PyPDF2
 import click
 import faiss
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
-
-import settings
-import llamafile_client as llamafile
-
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+import llamafile_client as llamafile
+import settings
+
 logger = logging.getLogger(__name__)
+
+
+def load_pdf(path: str) -> str:
+    with open(path, "rb") as f:
+        reader = PyPDF2.PdfFileReader(f)
+        text = []
+
+        for page in reader.pages:
+            text.append(page.extract_text())
+
+        return "".join(text)
 
 
 def chunk_text(text: str) -> Iterator[str]:
@@ -57,6 +67,10 @@ def load_data_for_indexing() -> Iterator[str]:
                 text = f.read()
                 for chunk in chunk_text(text):
                     yield chunk
+        for path in Path(directory).rglob("*.pdf"):
+            text = load_pdf(str(path))
+            for chunk in chunk_text(text):
+                yield chunk
 
 
 def embed(text: str) -> np.ndarray:

Mozilla-Ocho/llamafile-rag-example

(cherry picked from commit 045d4aca5fa3c8d4fb2d653836198b2c376e57b1)

app.py
@@ -1,22 +1,32 @@
 import json
 import logging
-import re
 import traceback
 from pathlib import Path
 from typing import Iterator
 
+import PyPDF2
 import click
 import faiss
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
-
-import settings
-import llamafile_client as llamafile
-
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+import llamafile_client as llamafile
+import settings
+
 logger = logging.getLogger(__name__)
+
+
+def load_pdf(path: str) -> str:
+    with open(path, "rb") as f:
+        reader = PyPDF2.PdfReader(f)
+        text = []
+
+        for page in reader.pages:
+            text.append(page.extract_text())
+
+        return "".join(text)
 
 
 def chunk_text(text: str) -> Iterator[str]:
@@ -57,6 +67,10 @@ def load_data_for_indexing() -> Iterator[str]:
                 text = f.read()
                 for chunk in chunk_text(text):
                     yield chunk
+        for path in Path(directory).rglob("*.pdf"):
+            text = load_pdf(str(path))
+            for chunk in chunk_text(text):
+                yield chunk
 
 
 def embed(text: str) -> np.ndarray: